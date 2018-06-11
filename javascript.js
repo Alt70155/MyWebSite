3fae39d28741bfb90546fc1c4fec24314db45d67
Alt70155 <chika1086@gmail.com>
2018-06-11 18:58:30 +0900

    fix header

---
+++ javascript.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let navText = () => {
+  document.getElementById("main_nav1").textContent = "HTML/CSS";
+  document.getElementById("main_nav2").textContent = "JavaScript";
+  document.getElementById("main_nav3").textContent = "サイト運営";
+  document.getElementById("main_nav4").textContent = "その他記事";
+}
+
+navText();
